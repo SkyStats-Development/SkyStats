303fc90bbb15cb7c226d210dfe1e685a283b0393
AxleTiger <bloodymountain@outlook.com>
2023-01-03 23:41:12 -0500

    Create bedwarsCommand.js

---
+++ src/discord/commands/bedwarsCommand.js
@@ -0,0 +1,67 @@
+//EZ GG WP wxkydoodle @ W4ckydoodle 4ever!!!!
+const hypixel = require('../../contracts/API/HypixelRebornAPI')
+const { addCommas } = require('../../contracts/helperFunctions')
+
+module.exports = {
+    name: 'bedwars',
+    description: 'Gets BedWars Data',
+    options: [
+        {
+            name: 'player',
+            description: 'Minecraft Username',
+            type: 3,
+            required: true
+        },  
+      ],
+
+    execute: async (interaction, client, InteractionCreate) => {
+        let play = interaction.options.getString('player')
+        const player = await hypixel.getPlayer(play)
+
+        const embed = {
+            title: `Bedwars Stats For ${player}`,
+            description: (`\n`),
+            fields: [
+            {
+                name: 'Level',
+                value: `[${player.stats.bedwars.level}✫]`,
+            },
+            {
+                name: `Final Kills`,
+                value: `${addCommas(player.stats.bedwars.finalKills)}`,
+            },
+            {
+                name: `FKDR`,
+                value: `${player.stats.bedwars.finalKDRatio}`,
+            },
+            {
+                name: `Wins`,
+                value: `${player.stats.bedwars.wins}`,
+            },
+            {
+                name: `WLR`,
+                value: `${player.stats.bedwars.WLRatio}`,
+            },
+            {
+                name: `Beds Broken`,
+                value: `${player.stats.bedwars.beds.broken}`,
+            },
+            {
+                name: `BBLR`,
+                value: `${player.stats.bedwars.beds.BLRatio}`,
+            },
+            {
+                name: `:sparkles: Winstreak :sparkles:`,
+                value: `${player.stats.bedwars.winstreak}`,
+            },
+        ],
+            timestamp: new Date().toISOString(),
+            footer: {
+                text: `🌹  SkyStats 🌹`,
+            },
+        };
+ 
+            await interaction.reply({ embeds:[embed] })
+
+    }
+};;
