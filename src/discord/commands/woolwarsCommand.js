558a516cc5a763f4559053c1d7189486284d272d
AxleTiger <bloodymountain@outlook.com>
2023-01-06 18:50:40 -0500

    update for star getting in woolwars to be fast

--- src/discord/commands/woolwarsCommand.js
+++ src/discord/commands/woolwarsCommand.js
@@ -2,16 +2,9 @@ const config = require('../../../config.json')
 const axios = require('axios')
 const { toFixed } = require('../../contracts/helperFunctions')
 const { getUUID } = require('../../contracts/API/PlayerDBAPI')
+const {getWoolWarsStar } = require(`../../../API/SkyStats-apis/API/skystats/getWWStar`)
 const { EmbedBuilder, ActionRowBuilder, SelectMenuBuilder, ButtonBuilder, ButtonStyle } = require('discord.js')
 
-function getWoolWarsStar(exp) {
-    const minimalExp = [0, 1e3, 3e3, 6e3, 1e4, 15e3];
-    const baseLevel = minimalExp.length;
-    const baseExp = minimalExp[minimalExp.length - 1];
-    if (exp >= baseExp) return (exp - baseExp) / 5e3 + baseLevel;
-    const lvl = minimalExp.findIndex((x)=>exp < x);
-    return lvl + exp / minimalExp[lvl];
-  }
 
 module.exports = {
     name: 'woolwars',
