7adb3c283b11a84f7d435a5452374b0f13e76532
AxleTiger <bloodymountain@outlook.com>
2023-01-07 20:49:13 -0500

    and my pc is on fire

--- src/discord/commands/kuudraCommand.js
+++ src/discord/commands/kuudraCommand.js
@@ -4,7 +4,8 @@ const messages = require('../../../messages.json')
 const { getUUID } = require('../../contracts/API/PlayerDBAPI')
 const {getLatestProfile} = require('../../../API/functions/getLatestProfile');
 const {addNotation,capitalize,addCommas} = require('../../contracts/helperFunctions')
-const config = require('../../../config.json')
+const config = require('../../../config.json');
+const { toLower } = require('lodash');
 
 module.exports = {
     name: 'kuudra',
@@ -20,43 +21,53 @@ module.exports = {
         
       ],
     execute: async (interaction, client, InteractionCreate) => {
-        let name = interaction.options.getString("name") || messages.defaultvalues.defaultname
-        const data = await getLatestProfile(name)
-        const uuid = await getUUID(name)
-        const profilename = (data.profileData.cute_name)
-        
-        const funny = (data.profileData.members)
-
-        console.log(funny)
-
+        interaction.reply({  content: "Loading Kuudra data" });
+        const name = interaction.options.getString("name") || messages.defaultvalues.defaultname
+        const profileraw = (await axios.get(`https://sky.shiiyu.moe/api/v2/profile/${name}`)).data.profiles
+        let currentProfile;
+        for (var key of Object.keys(profileraw)) {if (profileraw[key].current) currentProfile = key;}
+        const profilename = (profileraw[currentProfile].cute_name) || `ERROR404`
+        const t1comp = (profileraw[currentProfile].raw.nether_island_player_data.kuudra_completed_tiers.none)   || `0`
+        const t2comp = (profileraw[currentProfile].raw.nether_island_player_data.kuudra_completed_tiers.hot)    || `0`
+        const t3comp = (profileraw[currentProfile].raw.nether_island_player_data.kuudra_completed_tiers.burning)    || `0`
+        const t4comp = (profileraw[currentProfile].raw.nether_island_player_data.kuudra_completed_tiers.fiery)    || `0`
+        const t5comp = (profileraw[currentProfile].raw.nether_island_player_data.kuudra_completed_tiers.infernal)    || `0`
+        const t1wave = (profileraw[currentProfile].raw.nether_island_player_data.kuudra_completed_tiers.highest_wave_none)   || `0`
+        const t2wave = (profileraw[currentProfile].raw.nether_island_player_data.kuudra_completed_tiers.highest_wave_hot)    || `0`
+        const t3wave = (profileraw[currentProfile].raw.nether_island_player_data.kuudra_completed_tiers.highest_wave_burning)    || `0`
+        const t4wave = (profileraw[currentProfile].raw.nether_island_player_data.kuudra_completed_tiers.highest_wave_fiery)    || `0`
+        const t5wave = (profileraw[currentProfile].raw.nether_island_player_data.kuudra_completed_tiers.highest_wave_infernal)    || `0`
+        const pfmess = (profileraw[currentProfile].raw.nether_island_player_data.kuudra_party_finder.group_builder.note) || `Paper`
+        const pft = (profileraw[currentProfile].raw.nether_island_player_data.kuudra_party_finder.group_builder.tier) || `basic`
+        const pfreq = (profileraw[currentProfile].raw.nether_island_player_data.kuudra_party_finder.group_builder.combat_level_required) || `0`
         const embed = {
             title: `Kuudra Data For ${name} On ${profilename}`,
             URL: `https://sky.shiiyu.moe/stats/${name}`,
-            description: (`Party Finder Message: **text**\nParty Finder Requirement: **text**`),
+            description: (`Party Finder Message: **${pfmess}**\nParty Finder Requirement: **${pfreq}**\nParty Finder Tier: **${toLower(pft)}**`),
             fields: [
                 {
                     name: "Kuudra Tier 1",
-                    value: "Times Completed: \`number\`\nHighest Wave: \`number\`",
+                    value: `Times Completed: \`${t1comp}\`\nHighest Wave: \`${t1wave}\``,
                     inline: false
                 },
                 {
                     name: "Kuudra Tier 2",
-                    value: "Times Completed: \`number\`\nHighest Wave: \`number\`",
+                    value: `Times Completed: \`${t2comp}\`\nHighest Wave: \`${t2wave}\``,
                     inline: true
                 },
                 {
                     name: "Kuudra Tier 3",
-                    value: "Times Completed: \`number\`\nHighest Wave: \`number\`",
+                    value: `Times Completed: \`${t3comp}\`\nHighest Wave: \`${t3wave}\``,
                     inline: false
                 },
                 {
                     name: "Kuudra Tier 4",
-                    value: "Times Completed: \`number\`\nHighest Wave: \`number\`",
+                    value: `Times Completed: \`${t4comp}\`\nHighest Wave: \`${t4wave}\``,
                     inline: true
                 },
                 {
                     name: "Kuudra Tier 5",
-                    value: "Times Completed: \`number\`\nHighest Wave: \`number\`",
+                    value: `Times Completed: \`${t5comp}\`\nHighest Wave: \`${t5wave}\``,
                     inline: false
                 },
 
@@ -65,8 +76,7 @@ module.exports = {
             footer: {text: `${messages.footer.defaultbetter}`, iconURL: `${messages.footer.icon}`},
             };
 
-
-            await interaction.reply({  embeds: [ embed ] })
+            await interaction.editReply({  embeds: [ embed ] })
 
     }
 };;
