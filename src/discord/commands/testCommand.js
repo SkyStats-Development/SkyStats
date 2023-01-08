abbe000dac12c09283cfbd3a7b022016f1423899
AxleTiger <bloodymountain@outlook.com>
2023-01-07 20:49:04 -0500

    OK IT WORKS

--- src/discord/commands/testCommand.js
+++ src/discord/commands/testCommand.js
@@ -3,7 +3,7 @@ const { getLatestProfile } = require('../../../API/functions/getLatestProfile');
 const axios = require('axios');
 const { getUUID } = require('../../../src/contracts/API/PlayerDBAPI'); 
 const { SkyblockProfile } = require('hypixel-api-reborn'); //useless prob
-const { parseProfile } = require('../../../API/utils/hypixel'); // useless
+const { parseProfile } = require('../../../API/utils/hypixel'); 
 const config = require(`../../../config.json`);
 
 
@@ -22,22 +22,13 @@ module.exports = {
       ],
     execute: async (interaction, client, InteractionCreate) => {
         
-        let name = interaction.options.getString("string") || `Crimson_Armor` //default name to try the api
-        const uuid = getUUID(name) //unused, changes the name to uuid
-        const data = await getLatestProfile(name) // gets the latest skyblock profile data
-        const id = data.profileData.profile_id // gets the latest skyblock profile id
-        const profileraw = (await axios.get(`https://sky.shiiyu.moe/api/v2/profile/${name}`)).data.profiles // trying to get it to get all the raw data that it outputs for ONLY the profile id from above
-        
-
-    console.log (profileraw)
+        let name = interaction.options.getString("string") || `Crimson_Armor`
 
         
-    /*
-        If you find a way to get the profile id a different way (you need to find the profile string (example mmm3m3hy333h32h2208) with the line current = true or active = true)
-    */
+
 
         
-        await interaction.reply({content: `${profileraw}`, ephemeral: false})
+        await interaction.reply({content: `<:Barry_Century_Cake:944570165415251998>`, ephemeral: false})
 
     }
 };;
