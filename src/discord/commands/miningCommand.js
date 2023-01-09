286bfb8f74df7c62cce8ca6a52cadbf2c299ba2a
AxleTiger <bloodymountain@outlook.com>
2023-01-09 01:29:36 -0500

    mining :sob:

---
+++ src/discord/commands/miningCommand.js
@@ -0,0 +1,95 @@
+const { EmbedBuilder, ActionRowBuilder, SelectMenuBuilder, ButtonBuilder, ButtonStyle } = require('discord.js')
+const { getLatestProfile } = require('../../../API/functions/getLatestProfile');
+const { addNotation, addCommas } = require('../../contracts/helperFunctions')
+const { getNetworth, getPrices} = require('skyhelper-networth');
+const messages = require('../../../messages.json')
+const { default: axios } = require('axios');
+const wait = require('node:timers/promises').setTimeout;
+const { getUUID } = require('../../contracts/API/PlayerDBAPI')
+
+
+
+module.exports = {
+    name: 'mining',
+    description: 'Gets mining data on a player',
+    options: [
+        {
+            name: 'name',
+            description: 'mc username',
+            type: 3,
+            required: true
+        },
+
+        
+      ],
+    execute: async (interaction, client, InteractionCreate) => {
+        await interaction.deferReply();
+        await wait(1);
+        let name = interaction.options.getString("name")
+        const profileraw = (await axios.get(`https://sky.shiiyu.moe/api/v2/profile/${name}`)).data.profiles
+        let currentProfile;
+        for (var key of Object.keys(profileraw)) {
+            if (profileraw[key].current) currentProfile = key;}
+        const data = await getLatestProfile(name)
+        const profilename = (data.profileData.cute_name)
+        const player = (profileraw[currentProfile])
+        const hotmlvl = (player.data.mining.core.tier.level)
+        const xpForNext = (player.data.mining.core.tier.xp) || `0`
+        const xp = addCommas(xpForNext)
+        const progress = (player.data.mining.core.tier.progress) || `100%`
+        const hotmprogress = Math.round((progress) * 100) / 100
+        const milestone = (player.data.mining.commissions.milestone)
+        const completions = (player.data.mining.commissions.completions)
+        const completionscomma = addCommas(completions)
+        const pickaxe_ability = (player.data.mining.core.selected_pickaxe_ability)
+        
+
+        const chat = {
+            color: 0xffa600,
+            title: `${name}'s Mining Stats On ${profilename}`,
+            URL: `https://sky.shiiyu.moe/stats/${name}`,
+            description: (`${messages.errorcodes.error400}`),
+            thumbnail: {
+                url: `https://api.mineatar.io/body/full/${name}`,
+            },
+            timestamp: new Date().toISOString(),
+            fields: [
+                {
+                    name: "<:HeartOfTheMountain:1061814218598391818> HotM Level",
+                    value: `${hotmlvl}`,
+                    inline: true
+                },
+                {
+                    name: `<:iron_nugget:1061814731905716254> HotM Exp`,
+                    value: `${xp}`,
+                    inline: true
+                },
+                {
+                    name: `<:CrystalHollows:1061815055403974657> Progress Till Next Level`,
+                    value: `${hotmprogress}%`, //broken !! !!
+                    inline: true
+                },
+                {
+                    name: "<:Iron_Pickaxe:1061814368196628561> Commission Milestone",
+                    value: `${milestone}`,
+                    inline: true
+                },
+                {
+                    name: `<:ROYAL_PIGEON:1061815676282617896> Total Commissions`,
+                    value: `${completionscomma}`,
+                    inline: true
+                },
+                {
+                    name: `<:diamond_pickaxe:1061817609177288824> Pickaxe Ability`,
+                    value: `**${pickaxe_ability}**`, 
+                    inline: true
+                },
+            ],
+            footer: {text: `${messages.footer.defaultbetter}`, iconURL: `${messages.footer.icon}`},
+            };
+
+
+            await interaction.editReply({  embeds: [ chat ] })
+
+    }
+};;
