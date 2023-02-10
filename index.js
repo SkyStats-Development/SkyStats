ce4610300eb2511771c538b070c8cadf3a790837
AxleTiger <bloodymountain@outlook.com>
2023-02-10 14:29:03 -0500

    added... a panda?

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+const chalk = require("chalk");
+
+
+console.log(`───▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄───`)
+console.log(`───█▒▒░░░░░░░░░▒▒█───`)
+console.log(`────█░░█░░░░░█░░█────`)
+console.log(`─▄▄──█░░░▀█▀░░░█──▄▄─`)
+console.log(`█░░█─▀▄░░░░░░░▄▀─█░░█`)
+console.log(`───█░░░▀▄░░░▄▀░░░█───`)
+console.log(`───█░░░░░▀▀▀░░░░░█───`)
+console.log(`───█░░░░░░░░░░░░░█───`)
+console.log(`───█░░░░░░░░░░░░░█───`)
+console.log(`───▀█▄         ▄█▀──`)
+chalk.green(console.log(`Welcome to SkyStats v1.0.0 (Beta) Created by: Axle and the SkyStats Team`))
+
+
 process.on("uncaughtException", function (err) {
   console.log(err);
 });
