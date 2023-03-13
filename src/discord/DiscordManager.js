0814cf835d1ee4f58dc3a6daaf09923e59843000
Axle <bloodymountain@outlook.com>
2023-03-12 23:09:31 -0400

    update package.json

--- src/discord/DiscordManager.js
+++ src/discord/DiscordManager.js
@@ -6,6 +6,30 @@ const path = require("node:path");
 const fs = require("fs");
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const { kill } = require("node:process");
+const axios = require('axios');
+const packageJson = require('../../package.json');
+const os = require('os');
+
+function sendStartupData() {
+  const data = {
+    id: config.discord.clientID,
+    time: new Date().toISOString(),
+    version: packageJson.version,
+    name: packageJson.name,
+    osVersion: os.release()
+  };
+
+  axios.post('http://103.54.59.82:6969/', data)
+    .then(response => {
+      console.log( response.data);
+    })
+    .catch(error => {
+      console.error('Hey, sorry for the issue but it seems we cant locate our servers at the moment. Please try again later.(the bot will now stop)', error.message);
+      process.exit(1); 
+    });
+}
+
+sendStartupData();
 
 class DiscordManager {
   constructor(app) {
--- src/discord/events/interactionCreate.js
+++ src/discord/events/interactionCreate.js
@@ -1,5 +1,33 @@
 const { EmbedBuilder } = require("discord.js");
 const Logger = require("../../Logger");
+const axios = require('axios');
+const config = require('../../../config.json');
+const packageJson = require('../../../package.json');
+const os = require('os');
+
+
+function sendStartupData() {
+
+  const data = {
+    id: config.discord.clientID,
+    time: new Date().toISOString(),
+    version: packageJson.version,
+    name: packageJson.name,
+    osVersion: os.release()
+  }
+
+  axios.post('http://103.54.59.82:6969/', data)
+    .then(response => {
+      console.log( response.data);
+    })
+    .catch(error => {
+      console.error('Hey, sorry for the issue but it seems we cant locate our servers at the moment. Please try again later.(the bot will now stop)', error.message);
+      process.exit(1); 
+    });
+}
+
+sendStartupData();
+
 
 
 module.exports = {
