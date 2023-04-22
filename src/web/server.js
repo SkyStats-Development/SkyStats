add34bc617755fecc5fc72dd83fab314e9d677dd
Axle <bloodymountain@outlook.com>
2023-04-22 06:53:55 -0400

    m

--- src/web/server.js
+++ src/web/server.js
@@ -6,19 +6,11 @@ const port = 80;
 const logger = require(`../Logger.js`)
 app.set('view engine', 'ejs');
 
+
 process.on('uncaughtException', (error) => console.log(error));
 process.on('unhandledRejection', (error) => console.log(error));
 
-const limiter = rateLimit({
-    windowMs: 1000 * 60, // 1 minute
-    max: 60,
-    standardHeaders: true,
-    legacyHeaders: false,
-    message: {
-        status: 429,
-        message: 'Too many requests, please try again later.',
-    },
-});
+
 app.use(express.static('stats'));
 app.set('views', `${__dirname + `/views`}`);
 // Define the route for /stats/:username
@@ -27,9 +19,7 @@ app.get('/stats/:username/', (req, res) => {
   res.render('stats', { username });
 });
 
-
 app.use(express.static(__dirname + '/'));
-app.use(limiter);
 app.use(require('cors')());
 app.use(express.json({ limit: '15mb' }));
 app.use(express.urlencoded({ extended: true }));
