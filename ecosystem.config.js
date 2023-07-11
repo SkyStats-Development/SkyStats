180f269e12c66e0435c6d5a2571ee6c13754b69e
Axle <bloodymountain@outlook.com>
2023-07-11 19:11:08 -0400

    bump skyblock events

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,32 +1,32 @@
 module.exports = {
-    apps: [
-      {
-        name: 'SkyStatsPRODUCT',
-        script: 'index.js',
-        instances: 1,
-        autorestart: true,
-        watch: true,
-        max_memory_restart: '4G',
-        env: {
-          NODE_ENV: 'production'
-        },
-        env_production: {
-          NODE_ENV: 'production'
-        }
+  apps: [
+    {
+      name: "SkyStats-Production",
+      script: "index.js",
+      instances: 1,
+      autorestart: true,
+      watch: true,
+      max_memory_restart: "4G",
+      env: {
+        NODE_ENV: "production",
       },
-      {
-        name: 'SkyStatsDEV',
-        script: 'indexDEV.js',
-        instances: 1,
-        autorestart: true,
-        watch: true,
-        max_memory_restart: '4G',
-        env: {
-          NODE_ENV: 'development'
-        },
-        env_production: {
-          NODE_ENV: 'development'
-        }
-      }
-    ]
-  };
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "SkyStats-Development",
+      script: "indexDEV.js",
+      instances: 1,
+      autorestart: true,
+      watch: true,
+      max_memory_restart: "4G",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "development",
+      },
+    },
+  ],
+};
