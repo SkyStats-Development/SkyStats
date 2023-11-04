8c48518622eab094296ae5c601d5bee8b277bd72
Axle <95949634+AxleCat284@users.noreply.github.com>
2023-11-04 02:04:56 -0400

    🔁part 1 of update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "SkyStats-Production",
-      script: "index.js",
+      script: "./start/production_start.js",
       instances: 1,
       autorestart: true,
       watch: true,
@@ -16,7 +16,7 @@ module.exports = {
     },
     {
       name: "SkyStats-Development",
-      script: "indexDEV.js",
+      script: "./start/development_start.js",
       instances: 1,
       autorestart: true,
       watch: true,
