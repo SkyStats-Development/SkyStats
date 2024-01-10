0815ce34fdd780504eeba07f4d16491a8cd29208
Axle <95949634+AxleCat284@users.noreply.github.com>
2024-01-09 23:53:43 -0500

    Update handleUpdates.js

--- src/functions/handle/handleUpdates.js
+++ src/functions/handle/handleUpdates.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process');
 const Logger = require('../../Logger')
     function handleUpdates() {
-        exec('git pull', (stdout) => {
+        exec('git pull', (err, stdout, stderr) => {
             if (stdout.includes('Already up to date.')) Logger.updateGoodMessage("Already up to date!");
             else if (stdout.includes('Updating')) Logger.updateUpdatingMessage("Repository Updated from latest Github changes!");
         });
